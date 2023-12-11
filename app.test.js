266a9085089399645459c0afde59c45cffa8ed61
thatvirdiguy <amanCJ24@gmail.com>
2023-12-11 07:40:31 -0500

    unit testing v4

--- app.test.js
+++ app.test.js
@@ -6,3 +6,17 @@ describe("GET /", () => {
     expect("Hello, World!").toBe("Hello, World!")
   })
 })
+
+describe("POST /pythagoras/", () => {
+  it("test POST endpoint", async () => {
+	const response = await request(baseURL)
+  	.post("/pythagoras")
+  	.send({
+    	a: 3,
+    	b: 4
+  	})
+	expect(response.status).toBe(200);
+	expect(response.body.hypotenuse).toBe(5);
+  })
+})
+
